241bf03d4f716a0e5e49474f21ef9ef592001303
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-11-17 11:57:43 +0100

    Add image

--- app/src/main/java/fr/isep/blindtest_project/MainActivity.java
+++ app/src/main/java/fr/isep/blindtest_project/MainActivity.java
@@ -3,6 +3,7 @@ package fr.isep.blindtest_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.music);
+        imageView.setImageResource(R.drawable.music);
     }
 }
